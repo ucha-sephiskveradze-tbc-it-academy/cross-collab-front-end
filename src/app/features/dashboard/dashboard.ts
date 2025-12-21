ef3e385f9e8b7e911e68859304b62f9f7ad0e686
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2025-12-22 01:08:22 +0400

    feat(dashboard): trending implement, upcoming events limit implement

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { Header } from '../../shared/ui/header/header';
 import { Footer } from '../../shared/ui/footer/footer';
 import { IEventItem } from './model/event.mode';
@@ -6,6 +6,8 @@ import { CalendarIcon } from 'primeng/icons';
 import { CommonModule } from '@angular/common';
 import { Calendar } from './calendar/calendar';
 import { Button } from '../../shared/ui/button/button';
+import { EventService } from './services/events.service';
+import { httpResource } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,55 +16,22 @@ import { Button } from '../../shared/ui/button/button';
   styleUrl: './dashboard.scss',
 })
 export class Dashboard implements OnInit {
+  private eventService = inject(EventService);
   user = {
     name: 'Sarah',
     role: 'Employee',
   };
 
-  events = signal<IEventItem[]>([
-    {
-      id: 1,
-      title: 'Annual Team Building Summit',
-      date: '2025-01-18',
-      time: '09:00 - 17:00',
-      location: 'Grand Conference Hall',
-      category: 'Team Building',
-      description: 'Full day of engaging activities and workshops.',
-      status: 'Registered',
-      registeredCount: 142,
-      capacity: 150,
-      isTrending: false,
-    },
-    {
-      id: 2,
-      title: 'Leadership Workshop: Effective Communication',
-      date: '2025-01-20',
-      time: '14:00 - 16:30',
-      location: 'Training Room B',
-      category: 'Workshop',
-      description: 'Improve leadership and communication skills.',
-      status: 'Registered',
-      registeredCount: 28,
-      capacity: 30,
-      isTrending: false,
-    },
-    {
-      id: 3,
-      title: 'Happy Friday: Game Night',
-      date: '2025-01-24',
-      time: '18:00 - 21:00',
-      location: 'Recreation Lounge',
-      category: 'Happy Friday',
-      description: 'Games, snacks, and fun.',
-      status: 'Waitlisted',
-      registeredCount: 50,
-      capacity: 50,
-      isTrending: true,
-    },
-  ]);
-
-  upcomingEvents = this.events;
-  trendingEvents = signal(this.events().filter((e) => e.isTrending));
-
+  events = this.eventService.events;
+  upcomingEvents = computed(() =>
+    this.events.hasValue()
+      ? [...this.events.value()]
+          .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+          .slice(0, 5)
+      : []
+  );
+  trendingEvents = computed(() =>
+    this.events.hasValue() ? this.events.value().filter((e) => e.isTrending) : []
+  );
   ngOnInit() {}
 }
