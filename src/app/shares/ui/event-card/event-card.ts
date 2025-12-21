e25ab6f9341c96c51b08f02547cf5346c3763172
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2025-12-22 03:13:20 +0400

    feat(event-card): everything implemented except router to a button

--- src/app/shares/ui/event-card/event-card.ts
+++ src/app/shares/ui/event-card/event-card.ts
@@ -1,24 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { IEventItem } from './model/event.mode';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-event-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './event-card.html',
   styleUrl: './event-card.scss',
 })
 export class EventCard {
-  @Input({ required: true }) event!: {
-    id: number;
-    title: string;
-    date: string;
-    time: string;
-    location: string;
-    category: string;
-    description: string;
-    status: string;
-    registeredCount: number;
-    capacity: number;
-  };
+  @Input({ required: true }) event!: IEventItem;
 
   get spotsLeft(): number {
     return this.event.capacity - this.event.registeredCount;
