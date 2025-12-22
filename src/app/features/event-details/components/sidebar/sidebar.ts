56c1133a8bc11fd90a954ee938735014eb59a601
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2025-12-22 14:43:10 +0400

    feat(event-details): sidebar html-css implement

--- src/app/features/event-details/components/sidebar/sidebar.ts
+++ src/app/features/event-details/components/sidebar/sidebar.ts
@@ -1,12 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { IEventDetails } from '../../models/event-details.model';
+import { Button } from '../../../../shared/ui/button/button';
 
 @Component({
   selector: 'app-event-sidebar',
-  imports: [],
+  imports: [Button],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
   @Input({ required: true }) event!: IEventDetails;
+
+  onRegister() {
+    // TEMP (until backend is ready)
+    console.log('Register clicked for event:', this.event.id);
+
+    // FUTURE:
+    // this.eventDetailsService.register(this.event.id)
+  }
 }
