75748f28e776154560a4dc89b140ff98c7a88269
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2025-12-23 02:38:09 +0400

    feat(admin-details): ts implemented with mock-data, html created with primeNg-s

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,40 +9,54 @@ export const routes: Routes = [
     redirectTo: 'login',
     pathMatch: 'full',
   },
+
   {
     path: 'login',
     component: SignIn,
   },
+
   {
     path: 'sign-up',
     loadComponent: () => import('./features/auth/sign-up/sign-up').then((c) => c.SignUp),
   },
+
   {
     path: 'forgot-password',
     loadComponent: () =>
       import('./features/auth/forgot-password/forgot-password').then((c) => c.ForgotPassword),
   },
+
   {
     path: 'dashboard',
     // canActivate: [authGuard],
     loadComponent: () => import('./features/dashboard/dashboard').then((c) => c.Dashboard),
   },
+
   {
     path: 'events',
     // canActivate: [authGuard],
     loadComponent: () => import('./features/events/events').then((c) => c.Events),
   },
+
   {
     path: 'events/:id',
     // canActivate: [authGuard],
     loadComponent: () =>
       import('./features/event-details/event-details').then((c) => c.EventDetails),
   },
+
   {
     path: 'admin/main',
     // canActivate: [authGuard, adminGuard],
     loadComponent: () => import('./features/admin/main/main').then((c) => c.Main),
   },
+
+  {
+    path: 'admin/main/:id',
+    // canActivate: [authGuard, adminGuard],
+    loadComponent: () => import('./features/admin/main/components/view/view').then((c) => c.View),
+  },
+
   {
     path: '**',
     redirectTo: 'login',
