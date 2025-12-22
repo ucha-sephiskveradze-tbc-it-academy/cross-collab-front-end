24aad534eb8c8098459f20478c04c8e609ca6cc0
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2025-12-22 16:25:14 +0400

    fix(event-details): changed event-details.model and updated all childer html

--- src/app/features/event-details/models/event-details.model.ts
+++ src/app/features/event-details/models/event-details.model.ts
@@ -1,44 +1,25 @@
 export interface IEventDetails {
-  id: number;
+  eventId: number;
   title: string;
-  category: string;
   description: string;
-  fullDescription?: string;
 
-  date: string;
-  time: string;
+  startDateTime: string;
+  endDateTime: string;
+
   location: string;
-  duration: string;
+
+  category: {
+    categoryId: number;
+    categoryName: string;
+  };
 
   capacity: number;
-  registeredCount: number;
-  waitlistCount: number;
+  totalRegistered: number;
 
-  status: 'Open' | 'Registered' | 'Waitlisted';
-  registrationClosesAt: string;
+  currentUserStatus: 'REGISTERED' | 'CANCELLED' | 'NONE';
 
-  agenda: {
-    time: string;
-    title: string;
-    description: string;
-    tags?: string[];
-  }[];
-
-  speakers: {
-    name: string;
-    role: string;
-    bio: string;
-    avatarUrl?: string;
-  }[];
-
-  faqs: {
-    question: string;
-    answer: string;
-  }[];
-
-  organizer: {
-    name: string;
-    department: string;
-    email?: string;
-  };
+  // optional (details-only)
+  agenda?: any[];
+  speakers?: any[];
+  faqs?: any[];
 }
