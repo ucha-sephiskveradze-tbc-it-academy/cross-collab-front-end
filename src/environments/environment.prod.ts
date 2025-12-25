7d4d6be6f7b75d38b6a8914cca132005e5bc3f1b
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2025-12-25 12:34:20 +0400

    fix(environment): api fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.yourdomain.com',
   authApiUrl: 'https://unperfidiously-agrostographical-rudy.ngrok-free.dev/api',
   featureFlags: {
     enableMockData: false,
