ebd1ce6fbf3b119ca9ad649ca29029e327a76089
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2025-12-25 12:45:55 +0400

    environment prod change, max file size incr

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  apiUrl: 'https://unperfidiously-agrostographical-rudy.ngrok-free.dev/api',
   authApiUrl: 'https://unperfidiously-agrostographical-rudy.ngrok-free.dev/api',
   featureFlags: {
     enableMockData: false,
